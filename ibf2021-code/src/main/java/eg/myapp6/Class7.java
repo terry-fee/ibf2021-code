f6b276ea009bd742e22244f4da96213641c7480b
terry-fee <tf@somewhere.com>
2021-12-11 19:08:11 +0800

    Made changes

--- ibf2021-code/src/main/java/eg/myapp6/Class7.java
+++ ibf2021-code/src/main/java/eg/myapp6/Class7.java
@@ -7,22 +7,11 @@ import java.util.concurrent.Future;
 
 public class Class7 
 {
-
-    public Future<Integer> cube(Integer i) {
-        ExecutorService tp = Executors.newFixedThreadPool(2);
-        return tp.submit(() -> { 
-            return Integer.valueOf(i*i*i); }
-        );
-    }
-
     public static void main(String[] args) throws Exception {
-        /* ExecutorService threadPool = Executors.newFixedThreadPool(2);
-        Runnable r = () -> { 
-            System.out.println("Hello, World"); 
-    
-        };
-        threadPool.submit(r); */
-
-        System.out.println("Answer: " + new Class7().cube(3).get());
+        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        threadPool.submit( () -> { 
+            System.out.println("Hello, World"); }
+        ); 
+        threadPool.shutdown();
     }
 }
